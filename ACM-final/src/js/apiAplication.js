ce09c8606e4be094ba3d755aed61cd61e836b8e2
Sebaaa._.as <100049059+ITAD0RI-YUJI@users.noreply.github.com>
2024-12-10 23:10:24 -0500

    Moviendo archivos .js

---
+++ ACM-final/src/js/apiAplication.js
@@ -0,0 +1,29 @@
+import {getApiContent} from './apiCall.js';
+import {appendPost} from './append.js';
+import {tags} from './tags.js';
+import {users , getUserName} from './user-name.js';
+import {fecha , mesArray} from './fecha.js';
+ 
+const apiUrl = 'https://dummyjson.com/posts';
+
+let callApi = async url =>{
+    let getPost = await getApiContent(url);
+
+    return getPost;
+}
+
+async function useInfo(url){
+    let infoApi = await callApi(url);
+
+    let post = infoApi.posts;
+
+    post.forEach(postSelected => {
+        // console.log(tags(postSelected.tags));
+        console.log(getUserName(users));
+        const usuario = getUserName(users);
+        
+        appendPost(usuario.nombre , usuario.info.img , usuario.info.arroba , postSelected.id , postSelected.title , postSelected.body , postSelected.tags, 
+                    postSelected.reactions.likes , postSelected.reactions.dislikes , postSelected.views , fecha(mesArray));
+    });
+}
+useInfo(apiUrl);
---
+++ ACM-final/src/js/user-name.js
@@ -0,0 +1,165 @@
+export const users = {
+    "Monkey D. Luffy":{
+        img: "https://avatarfiles.alphacoders.com/364/364185.png",
+        arroba: "PirateKing"
+    },
+
+    "Naruto":{
+        img: "https://i.pinimg.com/200x150/a7/5e/9e/a75e9e35a8ed1ccc1017db40438c60e9.jpg",
+        arroba:"Konoha_Hokage"
+    },
+
+    "Saitama":{
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSXEJbxeETOBebjfA2G35PcjjP8oN3Sfg3RA&s",
+        arroba: "Pelón"
+    },
+
+    "Frieren":{
+        img: "https://avatarfiles.alphacoders.com/375/375161.png",
+        arroba: "Recolectora_de_Magia"
+    },
+
+    "Nakano Miku":{
+        img: "https://imagedelivery.net/LBWXYQ-XnKSYxbZ-NuYGqQ/7aea3130-5ad7-4101-a524-22bb0f3f7300/avatarhd",
+        arroba: "NakaMiku"
+    },
+
+    "Yor_Forger":{
+        img: "https://i.pinimg.com/736x/05/a5/9d/05a59d40eaa5796c251197bd283a8491.jpg",
+        arroba: "YorForger"
+    },
+
+    "Roronoa Zoro":{
+        img: "https://i.pinimg.com/736x/09/5c/44/095c44ce9421a10fad713a32d8648f6f.jpg",
+        arroba: "KingOfHell"
+    },
+
+    "Vinsmoke Sanji":{
+        img: "https://i.pinimg.com/736x/57/ae/eb/57aeebb178ae223c5380fe6793fb591f.jpg",
+        arroba: "Mr._Prince"
+    },
+
+    "Nami":{
+        img: "https://i.redd.it/7kf8vje5fa1d1.jpeg",
+        arroba: "Nami"
+    },
+
+    "Ichigo":{
+        img: "https://i.pinimg.com/564x/0e/1e/8f/0e1e8f714ed3a4afefe5ac920b16ebbc.jpg",
+        arroba: "Ichigo"
+    },
+
+    "Zero Two":{
+        img: "https://i.pinimg.com/originals/f4/6b/8d/f46b8d05e377c9a791b15f805f527345.jpg",
+        arroba: "002_Strelitzia"
+    },
+
+    "Asta":{
+        img: "https://forums.gamesamba.com/core/image.php?userid=3965402&dateline=1504811216",
+        arroba: "King_sorcerer"
+    },
+
+    "Nero":{
+        img: "https://cdn.pfps.gg/pfps/4873-black-clover-nero-secre-swallowtail.png",
+        arroba: "Nero_bird"
+    },
+
+    "Yami_Zukehiro":{
+        img: "https://i.pinimg.com/736x/fb/f0/3f/fbf03fadea683e9e91fc86eb9bad36ec.jpg",
+        arroba: "BlackBulls_Capitan"
+    },
+
+    "Gasai Yuno":{
+        img: "https://pbs.twimg.com/profile_images/1379136935810232320/Pq_qmdks_400x400.jpg",
+        arroba: "Yukis_gf"
+    },
+
+    "Fubuki":{
+        img: "https://i.pinimg.com/474x/91/18/65/91186582edf84a3ca093f9bdb7dea95d.jpg",
+        arroba: "Fubuki"
+    },
+
+    "Nakano Nino":{
+        img: "https://i.pinimg.com/736x/bf/ee/dd/bfeeddb1315d762bd897cd878dd55754.jpg",
+        arroba: "Nakano_nino"
+    },
+
+    "Nakano-Itsuki":{
+        img: "https://i.pinimg.com/736x/b1/8d/6a/b18d6a26d98e300ecdc6511e1d9d1975.jpg",
+        arroba: "Nakano_Itsuki"
+    },
+
+    "Jiraiya":{
+        img: "https://i.pinimg.com/236x/c1/73/7d/c1737d04cffd8badb9b7bd607dfbe305.jpg",
+        arroba: "Sabio_pervertido"
+    },
+
+    "Tsunade":{
+        img: "https://i.pinimg.com/736x/75/08/cd/7508cd0518d19311d32eb0d5b382c742.jpg",
+        arroba: "Tsunade_senju"
+    },
+
+    "Moa Kikuchi":{
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlqKelwR8Jyjwf_wJmCH3gyT2uIlNlQbgKeg&s",
+        arroba: "Moa-Metal_♫"
+    },
+
+    "Suzuka Nakamoto":{
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTspIQBlvtSVY3eAQJQd3cE-TFe6zzUJ28APA&s",
+        arroba: "Su-metal_♫"
+    },
+
+    "Momoko Okazaki":{
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV_yozbO_qYGmO4VjuC29mxy_f9WtLZyPj05WcukXjuanQG4oi",
+        arroba: "Momo-metal_♫"
+    },
+
+    "Gojo Satoru":{
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6-8_uaR_kpEEwq2EQuT-LWfy_r8Dd1ziEZw&s",
+        arroba: "KitKat"
+    },
+
+    "Suguro Geto":{
+        img: "https://i.pinimg.com/736x/1b/de/dc/1bdedc3493e6b05745ffc63a42c952c5.jpg",
+        arroba: "Monkey_hunter"
+    },
+
+    "Kugisaki Nobara":{
+        img: "https://i.pinimg.com/736x/4f/a4/6e/4fa46e2cb238e4df6f2fde35b95a8caa.jpg",
+        arroba: "Nobara"
+    },
+
+    "Itadori Yuuji":{
+        img: "https://64.media.tumblr.com/2bfbf93138a0bba37e648fb9cd5a7bca/c7da08e5430f04f1-ae/s540x810/4e26e49d14b1063dba4adf79b48ff30fb996d9a3.jpg",
+        arroba: "Itad0Ri__YuUjI"
+    },
+
+    "Fushiguro Megumi":{
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH5lWfjiD0v3GRQtWVdf1rOuZqNv76O1R7mw&s",
+        arroba: "Mahoraga_Lover"
+    },
+
+    "Denji":{
+        img: "https://avatarfiles.alphacoders.com/339/339627.jpg",
+        arroba: "ChainSawMan"
+    },
+
+    "Son Goku":{
+        img: "https://i.pinimg.com/originals/a0/34/4d/a0344df4339a0ec92aacfb635e1399ff.jpg",
+        arroba: "Kakaroto"
+    }
+}
+
+export function getUserName(usersObject){
+    let claves = Object.keys(usersObject);
+    let numRandom = Math.floor(Math.random() * claves.length);
+    let nombre = claves[numRandom];
+    let info = usersObject[claves[numRandom]]
+
+    return {nombre , info};
+}
+
+export function getUserInfo(userSelected){
+    let valores = Object.values(userSelected);
+    return valores
+}
